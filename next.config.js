bb2022c2d1abf2940ceaa9ce17c9b8c63326c3ac
Hadrian <hadrian@uchicago.edu>
2024-09-24 13:39:41 -0400

    tweak build stuff

--- next.config.js
+++ next.config.js
@@ -7,17 +7,8 @@ await import("./src/env.js");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
-
-  /**
-   * If you are using `appDir` then you must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
-  transpilePackages: ["geist"],
+  basePath: "/csil-website-experiment",
+  output: "export",
 };
 
 export default config;
